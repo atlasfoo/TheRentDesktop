a78f0c108153b35e041ff4920187ea01a5ea0cd4
thefoo@win <jf_mejiar547@live.com>
2019-07-20 22:47:44 -0600

    pend auto

--- SQLs/RentCarDML.sql
+++ SQLs/RentCarDML.sql
@@ -11,3 +11,26 @@ select @rold;
 call sp_login_sysuser('dmin', 'admin.123', @rolr);
 select @rolr;
 
+
+#categorias
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('A', 'City Car', 25);
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('B', 'Subcompacto', 30);
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('C', 'Compacto', 30);
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('D', 'Mediano', 40);
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('Pick Up E', 'Camioneta con tina eq. básica', 70);
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('Pick Up S', 'Camioneta con tina eq. completo', 80);
+insert into Categoria(Nombre, Descripcion, Costo_dia) values('Camioneta', 'Camioneta cerrada básica', 70);
+
+select * from Categoria;
+
+#modelos
+call sp_new_modelo('Toyota', 'Yaris', '1.3 VVTi', 1, 'Sedan', 16);
+call sp_new_modelo('Mazda', '2', '1.5l Skyactiv-G', 1, 'Sedan', 16);
+call sp_new_modelo('Chevrolet', 'Spark', '1.0 iDSi', 1, 'Hatchback', 15);
+call sp_new_modelo('Chevrolet', 'Beat', '1.4 DOHC', 1, 'Sedan', 16);
+call sp_new_modelo('Mitsubishi', 'L200', '2.5 TDi', 2, 'PickUp', 19);
+call sp_new_modelo('Mitsubishi', 'L200', '2.5 TDi', 2, 'PickUp', 20);
+call sp_new_modelo('Toyota', 'Corolla', '1.8 VVTi', 1, 'Sedan', 17);
+call sp_new_modelo('Nissan', 'Frontier NP-300', '3.0 TDi', 2, 'PickUp', 19);
+
+#auto
--- SQLs/RentCarSPs.sql
+++ SQLs/RentCarSPs.sql
@@ -1,4 +1,6 @@
 #TODO: procedimientos almacenados y funciones
+use bemorsa1qiar4u96lent;
+#nuevo empleado
 DELIMITER //
 Create PROCEDURE sp_new_empleado (in p_nom varchar(20), in s_nom varchar(20), in p_apll varchar(20), in s_apll varchar(20), in dir varchar(70), in ced varchar(16), in tel varchar(15), in correo varchar(50), out id_empl int)
 begin
@@ -12,6 +14,7 @@ drop column contraseña;
 alter table sysuser
 add column pswd varbinary(100);
 
+#usuario de sistema
 DELIMITER //
 CREATE PROCEDURE sp_new_sysuser (in id_empleado int, in usrname varchar(50), in pswd varchar(100), in rol int, out roln varchar(15))
 	begin
@@ -46,3 +49,38 @@ begin
 		set roln='DENEGADO';
 	end if;
 end //
+
+#modelo de auto
+
+DELIMITER //
+CREATE PROCEDURE sp_new_modelo(in marca varchar(30), in modelo varchar(20), in mot varchar(20), in comb int, in tipo_carroceria varchar(20), in id_cat int)
+begin
+	declare combs varchar(20);
+	if comb=1 then
+		set combs='GASOLINA';
+	elseif comb=2 then
+		set combs='DIESEL';
+	elseif comb=3 then
+		set combs='HIBRIDO';
+	else
+		set combs='GASOLINA';
+	end if;
+    insert into Modelo_Auto(Marca, Modelo, Motorizacion, Combustible, Tipo_Carroceria, Id_Categoria) values(marca, modelo, mot, combs, tipo_carroceria, id_cat);
+end//
+
+alter table Auto
+add column No_Chasis varchar(30);
+
+DELIMITER //
+CREATE PROCEDURE sp_new_auto(in placa varchar(10), in no_c varchar(30), in vin varchar(30), in color varchar(30), in trans int, in id_mod int)
+begin
+	declare transm varchar(20);
+	if trans=1 then
+		set transm='MANUAL';
+	elseif trans=2 then
+		set transm='AUTOMATICO';
+	else
+		set transm='MANUAL';
+	end if;
+    insert into Auto(Placa, Vin, Color, Transmisión, Id_Modelo, Estado, No_Chasis) values(placa, vin, color, transm, id_mod, 'DISPONIBLE', no_c);
+end//
