bbe7aae7ab3a9ee9d3ea6c10a5f9713ef21bc1be
sauliño <saulantonio0317@gmail.com>
2019-08-27 11:01:26 -0600

    add detalle

--- TheRent/src/therent/view/delivery/DeliveryWindowController.java
+++ TheRent/src/therent/view/delivery/DeliveryWindowController.java
@@ -1,5 +1,57 @@
 package therent.view.delivery;
 
-public class DeliveryWindowController {
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-}
+public class  DeliveryWindowController{
+
+@FXML
+private Tab tab_pane_entrega;
+
+@FXML
+private TableView<?> tabla_entrega;
+
+@FXML
+private TableColumn<?, ?> column_auto;
+
+@FXML
+private TableColumn<?, ?> column_año;
+
+@FXML
+private TableColumn<?, ?> column_color;
+
+@FXML
+private TableColumn<?, ?> column_placa;
+
+@FXML
+private TableColumn<?, ?> column_cliente;
+
+@FXML
+private Tab tab_pane_recepcion;
+
+@FXML
+private TableView<?> tabla_recepcion;
+
+@FXML
+private TableColumn<?, ?> column_auto2;
+
+@FXML
+private TableColumn<?, ?> column_año2;
+
+@FXML
+private TableColumn<?, ?> column_color2;
+
+@FXML
+private TableColumn<?, ?> column_placa2;
+
+@FXML
+private TableColumn<?, ?> column_cliente2;
+
+@FXML
+private JFXButton fx_button_entrega;
+
+        }
+
